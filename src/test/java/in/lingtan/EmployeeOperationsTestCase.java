ef4b04526f2f8b180f4e19a6cb218d3b84b6f20e
Lingtan1112 <77401106+Lingtan1112@users.noreply.github.com>
2021-05-05 13:40:56 +0530

    Updated project

--- src/test/java/in/lingtan/EmployeeOperationsTestCase.java
+++ src/test/java/in/lingtan/EmployeeOperationsTestCase.java
@@ -2,6 +2,7 @@ package in.lingtan;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -10,102 +11,176 @@ import org.junit.Test;
 
 public class EmployeeOperationsTestCase {
 
-static String name ;
-static String role ;
-static String email ;
-static String dob ;
-static String gender ;
-static String mobileNumber ;
-static String  joiningDate ;
-static String employeeId; 
+
 
 
 	@Test
 	public void addEmployeeTestCase1() { //Example data For employee 1
-		//System.out.println("addEmployeeTestCase1");
-		name = "JosephKuruvila";
-		role = "Software Developer";
-		employeeId = "Joseph2655";
-		email = "joseph@chainsys.com";
-		dob = "2000-02-12";
-		gender = "Male";
-		mobileNumber = "9600923846";
-		joiningDate = "2021-04-07";
-		///////////////////////////////////
-		EmployeeOperations.addEmployee(name,role,employeeId, email,dob,gender,mobileNumber,joiningDate);
+		EmployeeDomainClass employee1 = new EmployeeDomainClass();
+		employee1.name = "JosephKuruvila";
+		employee1.role = "Software Developer";
+		employee1.employeeID = "Joseph2655";
+		employee1.email = "joseph@chainsys.com";
+		employee1.dob = LocalDate.of(2000, 12, 12);
+		employee1.gender = "Male";
+		employee1.mobileNumber =9249324982L;
+		employee1.joiningData = LocalDate.of(2000, 12, 12);
+		
+		EmployeeOperations.addEmployee(employee1);
 		
 	}
 	@Test
 	public void addEmployeeTestCase2() { //Example datas For employee 2
+		EmployeeDomainClass employee2 = new EmployeeDomainClass();
+		employee2.name = "JD";
+		employee2.role = "Technical Consultant";
+		employee2.email = "jd@chainsys.com";
+		employee2.employeeID = "Jd2655";
+		employee2.dob = LocalDate.of(2000, 12, 12);
+		employee2.gender = "Male";
+		employee2.mobileNumber =Long.parseLong("9249324982");
+		employee2.joiningData = LocalDate.of(2000, 12, 12);
 		
-		name = "JD";
-		role = "Technical Consultant";
-		email = "jd@chainsys.com";
-		employeeId = "JD2655";
-		dob = "1999-04-14";
-		gender = "Male";
-		mobileNumber = "9249823982";
-		joiningDate = "2021-03-09";
-		///////////////////////////////////
-		EmployeeOperations.addEmployee(name,role,employeeId,email,dob,gender,mobileNumber,joiningDate);		
+		EmployeeOperations.addEmployee(employee2);
 		
 	}
 	@Test
 	public void addEmployeeTestCase3() { //Example datas For employee 2
+		EmployeeDomainClass employee3 = new EmployeeDomainClass();
+		employee3.name = "Lingtan";
+		employee3.role = "Ui designer";
+		employee3.email = "lingtan@chainsys.com";
+		employee3.employeeID = "Ling2655";
+		employee3.dob = LocalDate.of(2000, 12, 12);
+		employee3.gender = "Male";
+		employee3.mobileNumber =Long.parseLong("9249324982");
+		employee3.joiningData = LocalDate.of(2000, 12, 12);
 		
-		name = "Maran";
-		role = "HR";
-		employeeId = "Maran2655";
-		email = "maran@chainsys.com";
-		dob = "1980-04-14";
-		gender = "Male";
-		mobileNumber = "7689823982";
-		joiningDate = "2021-03-09";
-		///////////////////////////////////
-		EmployeeOperations.addEmployee(name,role,employeeId,email,dob,gender,mobileNumber,joiningDate);		
+		EmployeeOperations.addEmployee(employee3);
 		
 	}
 	
 	@Test
-	public void addEmployeeTestCase4() { //Example data For employee 1
-		//System.out.println("addEmployeeTestCase1");
-		name = "Scarlett";
-		role = "Functional Constultant";
-		employeeId = "Scarlett2655";
-		email = "scarlet@chainsys.com";
-		dob = "1960-02-02";
-		gender = "Female";
-		mobileNumber = "9605345345";
-		joiningDate = "2020-05-03";
-		///////////////////////////////////
-		HashMap<String,HashMap<String,String>> masterEmployeeData = EmployeeOperations.getMasterEmployeeDataHash();
-		System.out.println("Master Data after adding 4 Employee"+masterEmployeeData);
-		System.out.println("-------------------------------------------------------------------------------------------");
+	public void addEmployeeTestCase4() { 
+		EmployeeDomainClass employee4 = new EmployeeDomainClass();
+		employee4.name = "Scarlett";
+		employee4.role = "Functional Constultant";
+		employee4.employeeID = "Scarlett2655";
+		employee4.email = "scarlet@chainsys.com";
+		employee4.dob = LocalDate.of(2000, 12, 12);
+		employee4.gender = "Female";
+		employee4.mobileNumber =Long.parseLong("9249823982");
+		employee4.joiningData = LocalDate.of(2000, 12, 12);
+		EmployeeOperations.addEmployee(employee4);
+		
+		
+		HashMap<String, EmployeeDomainClass> masterEmployeeData = EmployeeOperations.getEmployeeMap();
+		System.out.println("Master Data after adding 4 Employee  :"+masterEmployeeData);
+		System.out.println("------------------------------------------------------------------------------------------------------------------------------");
+		
+	}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+
+	@Test  
+	public void deleteEmployeeTestCase1() {
+		
+		String employeeIdToDelete = "Ling2655";
+		boolean isRemoved = EmployeeOperations.deleteEmployee(employeeIdToDelete);
+		assertTrue(isRemoved);
 		
 	}
 	
-	
-	@Test  //To delete the data of an employee this method is used, when the position of the employee is given that is removed
-	public void deleteEmployeeTestCase1() { 
-				
-		String employeeToDelete = "JosephKuruvila";
-		EmployeeOperations.deleteEmployee(employeeToDelete);
+	@Test  
+	public void deleteEmployeeTestCase2() {
+		
+		String employeeIdToDelete = "Ling265";
+		boolean isRemoved = EmployeeOperations.deleteEmployee(employeeIdToDelete);
+		assertFalse(isRemoved);
 		
 	}
 	
-	@Test  //To delete the data of an employee this method is used, when the position of the employee is given that is removed
-	public void deleteEmployeeTestCase2() { 
-		String employeeToDelete = "Scarlett";
-		EmployeeOperations.deleteEmployee(employeeToDelete);
-		HashMap<String,HashMap<String,String>> masterEmployeeData = EmployeeOperations.getMasterEmployeeDataHash();
-		System.out.println("After deleting scarlett and josephKuruvila "+masterEmployeeData);
-		System.out.println("-------------------------------------------------------------------------------------------");
-	}
+
 	
 	@Test  //To delete the data of an employee this method is used, when the position of the employee is given that is removed
+	public void deleteEmployeeTestCase3() { 
+			String employeeIdToDelete = "Scarlett2655";
+			EmployeeOperations.deleteEmployee(employeeIdToDelete);
+			HashMap<String, EmployeeDomainClass> employeeMap = EmployeeOperations.getEmployeeMap();
+			System.out.println("After Deletion  :"+employeeMap);
+			System.out.println("--------------------------------------------------------------------------------------------------------------");
+	}
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Test 
 	public void viewAllDetailsOfEmployeeTestCase1() { 
-		String employeeName ="Maran";
-		HashMap<String,String> individualDataToDisplay = EmployeeOperations.viewAllDetailsOfEmployee(employeeName);
-		System.out.println(employeeName+ "'s Data alone"+"-->"+individualDataToDisplay);
-}
+		String employeeId ="Joseph2655";
+		EmployeeDomainClass individualDataToDisplay =  EmployeeOperations.viewAllDetailsOfEmployee(employeeId);
+		System.out.println(employeeId + "'s Data alone"+"-->"+individualDataToDisplay);
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+	}
+	
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Test 
+	public void editDetailsOfEmployeeTestCase2() { 
+		String employeeId ="Jd2655";
+		String newName = "JohnDavid";
+		EmployeeDomainClass editedDetailsOfEmployee =  EmployeeOperations.editEmployeeName(employeeId, newName);
+		System.out.println(employeeId + " After changing name :"+"-->"+editedDetailsOfEmployee);
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+	}
+	
+	public void editDetailsOfEmployeeTestCase3() { 
+		String employeeId ="Jd2655";
+		String newEmail = "JohnDavid@gmail.com";
+		EmployeeDomainClass editedDetailsOfEmployee =  EmployeeOperations.editEmployeeName(employeeId, newEmail);
+		System.out.println(employeeId + " After changing name :"+"-->"+editedDetailsOfEmployee);
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
+	}
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Test  
+	public void isEmployeeAvailableTestCase1() {
+		
+		String employeeIdToCheck = "Ling2655";
+		boolean isAvailable = EmployeeOperations.isEmployeeAvailable(employeeIdToCheck);
+		assertTrue(isAvailable);
+		
+	}
+	
+	@Test  
+	public void isEmployeeAvailableTestCase2() {
+		
+		String employeeIdToCheck = "Scarlett2655";
+		boolean isAvailable = EmployeeOperations.isEmployeeAvailable(employeeIdToCheck);
+		assertTrue(isAvailable);
+		
+	}
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Test
+	public void getEmployeeMap() {
+		HashMap<String, EmployeeDomainClass> employeeMap = EmployeeOperations.getEmployeeMap();
+		System.out.println("Employee Master Data  :"+employeeMap);
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------------------");
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	@Test
+	public void salaryOfEmployeeTestCase() { 
+		String employeeId ="Joseph2655";
+		int basicPay = 10000;
+		EmployeeDomainClass individualDataToDisplay =  EmployeeOperations.addSalary(employeeId,basicPay);
+		System.out.println("Salary-->"+individualDataToDisplay);
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
+	}
+	
+	
 }
